f26c7e0ef7fa4536fbd3186df6290fdf7448c594
Matej Gerek <matej.gerek@student.tuke.sk>
2024-10-20 20:09:37 +0200

    Export types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ import { useWhisper } from './useWhisper'
 
 export { useWhisper }
 export default useWhisper
+
+export type { UseWhisperConfig, UseWhisperReturn, UseWhisperTranscript, WhisperApiConfig } from './types'
